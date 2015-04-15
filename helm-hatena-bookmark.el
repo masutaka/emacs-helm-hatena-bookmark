6a7f2cc639fef2c60c42b8de4113108cd70c1857
Takashi Masuda <masutaka.net@gmail.com>
2015-04-16 00:28:37 +0900

    Bump version to 1.2.0

--- helm-hatena-bookmark.el
+++ helm-hatena-bookmark.el
@@ -4,7 +4,7 @@
 
 ;; Author: Takashi Masuda <masutaka.net@gmail.com>
 ;; URL: https://github.com/masutaka/emacs-helm-hatena-bookmark
-;; Version: 1.1.0
+;; Version: 1.2.0
 ;; Package-Requires: ((helm "1.6.9"))
 
 ;; This program is free software: you can redistribute it and/or modify
